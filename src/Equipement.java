66d1b25d6e673edf03229c643bede3f5fc0877c6
theomeuh <cnimporteki@hotmail.fr>
2019-10-13 22:05:08 +0200

    Lint of Equipement and Certificat classes
    Hotkey on eclipse is ctrl+maj+f

--- src/Equipement.java
+++ src/Equipement.java
@@ -6,31 +6,38 @@ public class Equipement {
 	private Certificat monCert; // Le certificat auto-signe.
 	private String monNom; // Identite de l’equipement.
 	private int monPort; // Le numéro de port d’ecoute.
-	
-	Equipement (String nom, int port) throws Exception {
-	// Constructeur de l’equipement identifie par nom
-	// et qui « écoutera » sur le port port.
+
+	Equipement(String nom, int port) throws Exception { // maybe specify which
+														// exception
+		// Constructeur de l’equipement identifie par nom
+		// et qui « écoutera » sur le port port.
 	}
+
 	public void affichage_da() {
-	// Affichage de la liste des équipements de DA.
+		// Affichage de la liste des équipements de DA.
 	}
+
 	public void affichage_ca() {
-	// Affichage de la liste des équipements de CA.
+		// Affichage de la liste des équipements de CA.
 	}
+
 	public void affichage() {
-	// Affichage de l’ensemble des informations
-	// de l’équipement.
+		// Affichage de l’ensemble des informations
+		// de l’équipement.
 	}
-	public String monNom (){
-	// Recuperation de l’identite de l’équipement.
+
+	public String monNom() {
+		// Recuperation de l’identite de l’équipement.
 		return monNom;
 	}
+
 	public PublicKey maClePub() {
-	// Recuperation de la clé publique de l’équipement.
+		// Recuperation de la clé publique de l’équipement.
 		return maCle.Publique();
 	}
+
 	public Certificat monCertif() {
-	// Recuperation du certificat auto-signé.
+		// Recuperation du certificat auto-signé.
 		return monCert;
 	}
 }
