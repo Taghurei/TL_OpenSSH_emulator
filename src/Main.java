f87cec49dc87e160441c6f827dcd9daedaa60dc2
theomeuh <cnimporteki@hotmail.fr>
2019-10-13 22:04:13 +0200

    PairesClesRSA class set up
    Test of the class in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		PaireClesRSA PCRSA = new PaireClesRSA();
+		System.out.println(PCRSA.Publique());
+		System.out.println(PCRSA.Privee());
 
 	}
 
--- src/PaireClesRSA.java
+++ src/PaireClesRSA.java
@@ -1,16 +1,38 @@
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 
 public class PaireClesRSA {
 	private KeyPair key;
-	
+
 	PaireClesRSA() {
-	// Constructeur : génération d’une paire de clé RSA.
+		// Constructeur : génération d’une paire de clé RSA.
+
+		// On va mettre un peu d'alea :
+		SecureRandom rand = new SecureRandom();
+		// On initialise la structure pour la generation de cle :
+		KeyPairGenerator kpg;
+		try {
+			kpg = KeyPairGenerator.getInstance("RSA");
+			// On definit la taille de cle :
+			kpg.initialize(512, rand);
+			// On genere la paire de cle :
+			KeyPair key = kpg.generateKeyPair();
+
+			this.key = key;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
 	}
+
 	public PublicKey Publique() {
 		return key.getPublic();
 	}
+
 	public PrivateKey Privee() {
 		return key.getPrivate();
 	}
