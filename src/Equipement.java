f10811e6d3ecd0c557cd1d76307ba57f057985dc
taghurei <gauthier.lacroix@supelec.fr>
2019-10-16 15:51:07 +0200

    seance 3

--- src/Equipement.java
+++ src/Equipement.java
@@ -8,8 +8,10 @@ import java.net.Socket;
 import java.security.PublicKey;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.Scanner;
 
 import org.bouncycastle.cert.CertException;
+import java.util.Scanner;
 
 public class Equipement {
 
@@ -82,6 +84,8 @@ public class Equipement {
 	}
 
 	public void server() {
+		Scanner scan = new Scanner(System.in);
+
 		System.out.println("Je suis un serveur (" + this.monNom + ") qui écoute sur " + this.monPort);
 
 		ServerSocket serverSocket = null;
@@ -101,13 +105,13 @@ public class Equipement {
 		// Attente de connextions
 		try {
 			NewServerSocket = serverSocket.accept();
+			
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-		System.out.println("Je suis connecté");
-
+			
+		System.out.println("J'ai reçu une connexion");
 		// Creation des flux natifs et evolues
 		try {
 			NativeIn = NewServerSocket.getInputStream();
@@ -121,7 +125,7 @@ public class Equipement {
 		// Reception d’un String
 		try {
 			String res = (String) ois.readObject();
-			System.out.println("Je reçois:" + res);
+			System.out.println("le client suivant souhaite se connecter:" + res);
 		} catch (ClassNotFoundException | IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -129,13 +133,62 @@ public class Equipement {
 
 		// Emission d’un String
 		try {
-			oos.writeObject("Au revoir");
+			oos.writeObject("Je suis le serveur : " + this.monNom);
 			oos.flush();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
+		ConnexionClient: while (true) {
+			System.out.println("On imagine ici que je n'ai pas le certificat (a implementer)");
+			System.out.println("Voulez vous accepter la connexion entrante? (y/n)");
+			String accept = scan.next();
+			switch (accept) {
+			case "y":
+				try {
+					oos.writeObject("connexion acceptée");
+					oos.flush();
+					try {
+						String res = (String) ois.readObject();
+						System.out.println("le client a donné la réponse suivante:" + res);
+						if (res.equals("connexion acceptée")) {
+							System.out.println("Nous avons tous les 2 accepté la connexion"
+									+ " nous pouvons echanger nos certificats");
+						}
+						else {
+							System.out.println("le serveur a refusé la connexion");
+						}
+					} catch (ClassNotFoundException | IOException e1) {
+						System.out.println("echec de connexion");
+						e1.printStackTrace();
+					}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				// Cas ou l'utilisateur accepte la connexion
+				break ConnexionClient;
+			case "n":
+				try {
+					oos.writeObject("connexion refusée");
+					oos.flush();
+					System.out.println("J'ai refusé la connexion"
+							+ " nous n'echangeons pas nos certificats");
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}			
+				break ConnexionClient;
+			default:
+				System.out.println("Commande inconnue");
+				break;
+			}
+		}
+		
+		
+		
+		
+		
 		// Fermeture des flux evolues et natifs
 		try {
 			ois.close();
@@ -145,7 +198,6 @@ public class Equipement {
 		} catch (IOException e) {
 			// Gestion des exceptions
 		}
-
 		// Fermeture de la connexion
 		try {
 			NewServerSocket.close();
@@ -162,6 +214,7 @@ public class Equipement {
 	}
 
 	public void client(String ServerName, int ServerPort) {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Je me connecte en tant que client à " + ServerName + " port " + ServerPort);
 		Socket clientSocket = null;
 		InputStream NativeIn = null;
@@ -176,8 +229,8 @@ public class Equipement {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-		System.out.println("Je suis connecté");
+		
+		System.out.println("Tentative de connexion...");
 
 		// Creation des flux natifs et evolues
 		try {
@@ -189,26 +242,72 @@ public class Equipement {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
+		
 		// Emission d’un String
 		try {
-			oos.writeObject("Bonjour, je suis " + this.monNom);
+			oos.writeObject("je suis le client:  " + this.monNom);
 			oos.flush();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
+		
 		// Reception d’un String
 		String res;
 		try {
 			res = (String) ois.readObject();
-			System.out.println("Je reçois:" + res);
+			System.out.println("le serveur est :" + res);//nom du serveur
 		} catch (ClassNotFoundException | IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
+		ConnexionServer: while (true) {
+			System.out.println("On imagine ici que je n'ai pas le certificat (a implementer)");
+			System.out.println("Voulez vous confirmer la connexion au serveur? (y/n)");
+			String accept = scan.next();
+			switch (accept) {
+			case "y":
+				try {
+					oos.writeObject("connexion acceptée");
+					oos.flush();
+					try {
+						String resAnswer = (String) ois.readObject();
+						System.out.println("le serveur a donné la réponse suivante:" + resAnswer);
+						if (resAnswer.equals("connexion acceptée")) {
+							System.out.println("Nous avons tous les 2 accepté la connexion"
+									+ " nous pouvons echanger nos certificats");
+						}
+					} catch (ClassNotFoundException | IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				// Cas ou l'utilisateur accepte la connexion
+				break ConnexionServer;
+			case "n":
+				try {
+					oos.writeObject("connexion refusée");
+					oos.flush();
+					System.out.println("J'ai refusé la connexion"
+							+ " nous n'echangeons pas nos certificats");
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}			
+				break ConnexionServer;
+			default:
+				System.out.println("Commande inconnue");
+				break;
+			}
+		}
+		
+		
+	
+		
+		// Emission d’un String
 		// Fermeture des flux evolues et natifs
 		try {
 			ois.close();
